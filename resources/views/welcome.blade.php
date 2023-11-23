03471a31ccc095c3f34f437fde577eb30cb5aeb9
Jacob Deepu <d2jdeepu@gmail.com>
2023-11-23 22:09:59 +0530

    Increase instruction pop-up width

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,7 +49,7 @@
                     <div class="fixed inset-0 bg-gray-500 bg-opacity-40 transition-opacity" aria-hidden="true" x-cloak x-show="modelOpen"
                         x-transition:enter="transition ease-out duration-300 transform" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
                         x-transition:leave="transition ease-in duration-200 transform" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"></div>
-                    <div class="my-20 inline-block w-full max-w-2xl transform overflow-hidden rounded-lg bg-white p-8 text-left shadow-xl transition-all 2xl:max-w-2xl" x-cloak x-show="modelOpen"
+                    <div class="my-20 inline-block w-full max-w-4xl transform overflow-hidden rounded-lg bg-white p-8 text-left shadow-xl transition-all 2xl:max-w-2xl" x-cloak x-show="modelOpen"
                         x-transition:enter="transition ease-out duration-300 transform" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="transition ease-in duration-200 transform"
                         x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
