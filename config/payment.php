34ec6f690b070584b7f4629f5a47bb5882d2f601
Jacob Deepu <d2jdeepu@gmail.com>
2024-08-02 15:38:05 +0530

    Add a custom payment config for production
         All the payment gateway sensitive data will be in loaded from the env file.

---
+++ config/payment.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /**
+     * Username
+     */
+    'username' => env('PAYMENT_USERNAME'),
+
+    /**
+     * Password
+     */
+    'password' => env('PAYMENT_PASSWORD'),
+
+    /**
+     * Product Id
+     */
+    'product_id' => env('PAYMENT_PRODUCT_ID'),
+
+    /**
+     * AES Encryption / Request key
+     */
+    'enc_request_key' => env('PAYMENT_ENC_REQUEST_KEY'),
+
+    /**
+     * AES Decryption / Response key
+     */
+    'dec_response_key' => env('PAYMENT_DEC_RESPONSE_KEY'),
+
+    /**
+     * Payment Url
+     */
+    'api_url' => env('PAYMENT_API_URL'),
+
+    /**
+     * JAvascript Loader Url
+     */
+    'js_url' => env('PAYMENT_JS_URL'),
+    
+];
