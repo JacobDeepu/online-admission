e8650ed4c7ec02d636378b69609dd043fa1e0d6c
Jacob Deepu <d2jdeepu@gmail.com>
2024-08-06 10:29:15 +0530

    Update retry-payment to handle dispatched event

--- resources/views/livewire/retry-payment.blade.php
+++ resources/views/livewire/retry-payment.blade.php
@@ -1,5 +1,5 @@
 <div class="jetstream-modal fixed inset-0 z-50 overflow-y-auto px-4 py-6 sm:px-0" style="display: none;" x-on:close.stop="retryPaymentModelOpen = false"
-    x-on:keydown.escape.window="retryPaymentModelOpen = false" x-show="retryPaymentModelOpen">
+    x-on:keydown.escape.window="retryPaymentModelOpen = false" x-show="retryPaymentModelOpen" @payment-processed="handlePaymentToken($event.detail.jsonData)">
     <div class="fixed inset-0 transform transition-all" x-show="retryPaymentModelOpen" x-on:click="retryPaymentModelOpen = false" x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
         <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
@@ -31,7 +31,13 @@
                     {{ __('Submit') }}
                 </x-button>
             </div>
+            <div class="fixed bottom-0 left-0 right-0 top-0 z-50 h-screen w-full flex-col items-center justify-center overflow-hidden bg-gray-700 opacity-75" wire:loading.flex>
+                <svg class="mr-2 inline h-24 w-24 animate-spin fill-blue-600 text-center text-gray-200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+                    <path
+                        d="M136,32V64a8,8,0,0,1-16,0V32a8,8,0,0,1,16,0Zm88,88H192a8,8,0,0,0,0,16h32a8,8,0,0,0,0-16Zm-45.09,47.6a8,8,0,0,0-11.31,11.31l22.62,22.63a8,8,0,0,0,11.32-11.32ZM128,184a8,8,0,0,0-8,8v32a8,8,0,0,0,16,0V192A8,8,0,0,0,128,184ZM77.09,167.6,54.46,190.22a8,8,0,0,0,11.32,11.32L88.4,178.91A8,8,0,0,0,77.09,167.6ZM72,128a8,8,0,0,0-8-8H32a8,8,0,0,0,0,16H64A8,8,0,0,0,72,128ZM65.78,54.46A8,8,0,0,0,54.46,65.78L77.09,88.4A8,8,0,0,0,88.4,77.09Z">
+                    </path>
+                </svg>
+            </div>
         </form>
-        
     </div>
 </div>
